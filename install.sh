42ff1de40a9e00bf9f0c9cc0fe0de02d6d5241d8
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-02-18 02:13:35 -0800

    more install

--- install.sh
+++ install.sh
@@ -3,11 +3,20 @@
 FOLDER=$(dirname $(realpath "$0"))
 cd $FOLDER
 
+# libraries to interface with the ADC and IMU respectivly
 yes | sudo pip3 install adafruit-circuitpython-ads1x15
 yes | sudo pip3 install adafruit-circuitpython-mpu6050
 
+# library to generate nice pwm for the servos and motors
 sudo apt-get install -y pigpio python-pigpio python3-pigpio
 
+# the communication library we use
+yes | sudo pip3 install UDPComms
+
+# VPN which will hopefully allow us access across the internet
+curl -s https://install.zerotier.com | sudo bash
+
+# installs the .service scripts
 for file in *.service; do
     [ -f "$file" ] || break
     sudo ln -s $FOLDER/$file /lib/systemd/system/
@@ -15,4 +24,5 @@ done
 
 sudo systemctl daemon-reload
 sudo systemctl enable monitor_battery # starts program on boot
-sudo systemctl enable pigpiod
+sudo systemctl enable pigpiod # starts program on boot
+
